d33964fe45b2d966f3f28f613e1aec76028287cd
nbeloglazov <nikelandjelo@gmail.com>
2010-05-27 00:30:16 +0300

    Tests for core.clj. Eventually...

--- test/snakejure/core_test.clj
+++ test/snakejure/core_test.clj
@@ -2,5 +2,41 @@
   (:use [snakejure.core] :reload-all)
   (:use [clojure.test]))
 
-(deftest replace-me ;; FIXME: write
-  (is false))
+(deftest add-points-t
+    (is (= [1 1] (add-points [0 25] [1 -3] [0 -21]))))
+
+(deftest overlaps-body?-t
+  (let [snake {:body [ [0 0] [0 1] [1 1] [2 1] [3 1] [3 2] ]}] 
+    (is (true? (overlaps-body? snake [1 1])))
+    (is (false? (overlaps-body? snake [3 3])))))
+
+(defn point? [point]
+  (and (< (point 1) width)
+       (< (point 0) height)
+       (>= (point 1) 0)
+       (>= (point 0) 0)))
+
+(defn apple? [{:keys [location color type]}]
+  (and (= :apple type)
+       (point? location)
+       (instance? java.awt.Color color)))
+
+(defn create-full-table-snake[]
+  (for [y (range height) x (range width)] [y x]))
+
+(deftest create-apple-t 
+  (is (every? apple?  (take 20 (repeatedly create-apple))))
+  (is (let [snake {:body (rest (create-full-table-snake))}
+	    apple (create-apple snake)]
+	(and (apple? apple)
+	     (= (:location apple) [0 0])))))
+
+(defn noiser? [{:keys [location color-silence color-noise type] }]
+  (and (point? location)
+       (instance? java.awt.Color color-silence)
+       (instance? java.awt.Color color-noise)
+       (= :noiser type)))
+
+(deftest create-noiser-t
+  (is (every? noiser? (take 20 (repeatedly create-noiser)))))
+
