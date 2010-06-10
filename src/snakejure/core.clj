3cf860ce8398ec3fd40742bf9c8764b6b1e4865f
nbeloglazov <nikelandjelo@gmail.com>
2010-06-10 23:12:58 +0300

    Adding levels. There is one level: basic level right now.

--- src/snakejure/core.clj
+++ src/snakejure/core.clj
@@ -1,9 +1,7 @@
 (ns snakejure.core
-    (:use (clojure.contrib seq-utils
-			   import-static))
-    (:import (java.awt Color)))
-
-(import-static java.awt.event.KeyEvent VK_LEFT VK_RIGHT VK_DOWN VK_UP)
+  (:use (clojure.contrib seq-utils
+			 import-static))
+  (:import (java.awt Color)))
 
 (def width 30)
 (def height 30)
@@ -11,11 +9,10 @@
 (def speed 100)
 (def noisers-num 4)
 (def walls-num 20)
-(def dirs { VK_LEFT [0 -1]
-	    VK_RIGHT [0 1]
-	    VK_UP [-1 0]
-	    VK_DOWN [1 0]} )
-
+(def dirs {:left  [0 -1]
+	   :right [0 1]
+	   :up [-1 0]
+	   :down [1 0]} )
 
 (defn overlaps-body? 
   "Check, if snake's body overlaps some given point."
@@ -36,52 +33,54 @@
   "Creates random point with x in [0,width), y in [0, height).
   If walls is given, then created point won't overlap them."
   ([]
-   [(rand-int height) (rand-int width)])
+     [(rand-int height) (rand-int width)])
   ([walls]
-   (find-first #(not (overlaps-walls? walls %)) (repeatedly random-point))))
+     (find-first #(not (overlaps-walls? walls %)) (repeatedly random-point))))
 
 (defn create-apple 
   "Creates new apple in random point. 
   Created apple won't overlap snake's body and walls."
   [snake walls] 
-   {:location (find-first #(not (overlaps-body? snake %)) (repeatedly #(random-point walls)))
-    :type :apple
-    :color (Color/RED)})
+  {:location (find-first #(not (overlaps-body? snake %)) (repeatedly #(random-point walls)))
+   :type :apple
+   :color (Color/RED)})
 
-(defn create-wall
+#_(defn create-wall
   "Creates new wall in random point."
   []
   {:location (random-point)
    :type :wall
    :color (Color/BLACK)})
 
-(defn create-walls [n]
+#_(defn create-walls
   "Creates sequence of n walls in random points.
   Walls may overlap each other"
+  [n]
   (take n (repeatedly create-wall)))
 
 
-(defn create-noiser [walls]
+#_(defn create-noiser
   "Creates new noiser in random location.
   It won't overlap walls."
+  [walls]
   {:location (random-point walls)
    :type :noiser
    :color-noise (Color/BLUE)
    :color-silence (Color/MAGENTA)})
 
-(defn create-noisers
+#_(defn create-noisers
   "Creates sequence with n noisers in random locations. 
   Noisers may overlap each other."
   [n walls]
   (take n (repeatedly #(create-noiser walls))))
 
-(defn create-snake
+#_(defn create-snake
   "Creates new snake of length 1 in random location. And direction to right.
   Snake's head won't overlap walls."
   [walls] 
   {:body (list (random-point walls))
    :type :snake
-   :dir VK_RIGHT
+   :dir :right
    :color (Color/CYAN)})
 
 (defn normalize-point
@@ -95,7 +94,9 @@
   snake will grow, length will increase by 1 unit."
   [{:keys [body dir] :as snake} & grow]
   (assoc snake :body  (cons (normalize-point (add-points (first body) (dirs dir)))
-			    (if grow body (butlast body)))))
+			    (if grow
+			      body
+			      (butlast body)))))
 
 (defn eats?
   "Checks if snake can ear appple."
@@ -119,17 +120,6 @@
   "Check if use win. User wins, when snake lay on every noser simultaneously."
   [{body :body} noisers]
   (every? true? (map #(includes? body (:location %)) noisers)))
-
-(defn reset 
-  "Takes refs of snake, apple, noisers and resets game. 
-  It will set to refs new generated snake, apple and ref. 
-  Snake will be 1 unit length."
-  [snake apple noisers walls]
-  (dosync 
-   (ref-set walls (create-walls walls-num))
-   (ref-set snake (create-snake @walls))
-   (ref-set apple (create-apple @snake @walls))
-   (ref-set noisers (create-noisers noisers-num @walls))))
 
 (defn update-direction 
   "Takes refs of snake, and set new snake with updated direction to it."
--- src/snakejure/ui.clj
+++ src/snakejure/ui.clj
@@ -1,13 +1,32 @@
 (ns snakejure.ui
     (:use (clojure.contrib import-static)
 	  (snakejure core))
+    (:require   (snakejure.levels basic))
     (:import (java.awt Dimension)
 	     (java.awt.event ActionListener KeyListener)
 	     (javax.swing JPanel Timer JFrame JOptionPane)))
 
-(import-static java.awt.event.KeyEvent VK_SPACE)
+(import-static java.awt.event.KeyEvent VK_SPACE VK_LEFT VK_RIGHT VK_UP VK_DOWN)
 
-(def dir-keys (set (keys dirs)))
+(def key-to-dir {VK_RIGHT :right
+		 VK_LEFT :left
+		 VK_UP :up
+		 VK_DOWN :down})
+(def dir-keys (set (keys key-to-dir)))
+
+(defn reset
+  "Takes refs of snake, apple, noisers and resets game.
+  It will set to refs new generated snake, apple and ref.
+  Snake will be 1 unit length."
+  [snake apple noisers walls]
+  (let [level (snakejure.levels.basic/create-level)]
+    (dosync
+     (ref-set walls (level :walls))
+     (ref-set snake (level :snake))
+     (ref-set apple (level :apple))
+     (ref-set noisers (level :noisers)))))
+
+
 
 (defn draw-point 
   "Draws point on given graphics g."
@@ -72,20 +91,21 @@
 		     (if (= (.getKeyCode e) VK_SPACE)
 		       (switch-timer timer))
 		     (if (dir-keys (.getKeyCode e))
-		       (update-direction snake (.getKeyCode e))))
+		       (update-direction snake (key-to-dir (.getKeyCode e)))))
 	 (keyReleased [e])
 	 (keyTyped [e])))
 
 (defn game []
   "Starts game. It will create frame with walls, snake, apple and noisers in it."
-  (let [walls (ref (create-walls walls-num))
-	snake (ref (create-snake @walls))
-	apple (ref (create-apple @snake @walls))
-	noisers (ref (create-noisers noisers-num @walls))
+  (let [walls (ref nil)
+	snake (ref nil)
+	apple (ref nil)
+	noisers (ref nil)
 	frame (JFrame. "Snake")
 	panel (game-panel frame snake apple noisers walls)
 	timer (Timer. speed panel)
- 	key-listener (create-key-listener snake timer)]
+	key-listener (create-key-listener snake timer)]
+    (reset snake apple noisers walls)
     (doto panel
       (.setFocusable true)
       (.addKeyListener key-listener))
