20491f0e63c48e006a00ad0dd44d5c7cf2cf82b5
nikelandjelo <nikelandjelo@nikelandjelo-laptop.(none)>
2010-07-24 00:00:28 +0300

    Adding swank dependency for convenience.

--- project.clj
+++ project.clj
@@ -2,4 +2,5 @@
   :description "Snake game written in clojure"
   :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-		 [log4j/log4j "1.2.16"]])
+		 [log4j/log4j "1.2.16"]]
+  :dev-dependencies [[swank-clojure "1.2.0"]])
