5af5b73a051e5ac1160d1e66a19cff1248b48405
Nikita Beloglazov <nikelandjelo@gmail.com>
2010-07-31 00:00:22 +0300

    Added results.clj file. Now all level wins saves in user's home directory in .snakejure file. Also menu jlist internal structure modified. Now it's date vector [level-name result] instead of string level-name and added CellRenderer to build JPanel using vector.

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject snakejure "0.1"
   :description "Snake game written in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-master-SNAPSHOT"]
-                 [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
+  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
+                 [org.clojure/clojure-contrib "1.2.0-RC1"]
 		 [log4j/log4j "1.2.16"]]
   :dev-dependencies [[swank-clojure "1.2.0"]])
--- src/snakejure/menu_gui.clj
+++ src/snakejure/menu_gui.clj
@@ -1,29 +1,49 @@
 (ns snakejure.menu-gui
-    (:import (javax.swing JFrame JList JOptionPane)
+    (:import (javax.swing JFrame JList JLabel JOptionPane JPanel ListCellRenderer)
 	     (java.awt.event KeyAdapter KeyEvent)
-	     (java.awt Dimension)
+	     (java.awt Dimension Color)
 	     (org.apache.log4j Logger FileAppender SimpleLayout Level))
     (:use [snakejure.level-loader :only (get-levels-map)]
-	  [snakejure.game-gui :only (create-game-panel)]))
+	  [snakejure.game-gui :only (create-game-panel)]
+	  [snakejure.results :only (update-results get-results-map)]))
 
-(def width 100)
-(def height 400)
+(def #^{:private true} width 300)
+(def #^{:private true} height 400)
 (def *log-file* "log.txt")
 (def *log-level* (Level/TRACE))
 
-(defn init-logging 
-  "Inits logging to log all level changes to *log-file*"
+(defn- init-logging 
+  "Inits logging to log it to *log-file*"
   []
   (doto (Logger/getRootLogger)
     (.addAppender (FileAppender. (SimpleLayout.) *log-file* false))
     (.setLevel *log-level*)))
-  
 
-(defn- create-poor-jlist []
+(defn- get-level-vectors
+  "Create vector of levels + according result"
+  []
+  (let [level-names (sort (keys (get-levels-map)))
+	results (get-results-map)]
+    (for [name level-names] [name (results name)])))
+
+(defn- create-list-renderer []
+  (proxy  [ListCellRenderer] []
+    (getListCellRendererComponent [list [name result] index is-sel has-focus]
+				  (doto (JPanel.)
+				    (.setName name)
+				    (.add (JLabel. name))
+				    (.setBackground (if is-sel
+						      (Color/LIGHT_GRAY)
+						      (Color/WHITE)))))))
+
+    
+
+(defn- create-poor-jlist
   "Creates Jlist without any listeners."
-  (let [level-names (apply sorted-set (keys (get-levels-map)))]
-    (doto (JList. (to-array level-names))
-      (.setSelectedIndex 0))))
+  []
+  (doto (JList. (to-array (get-level-vectors)))
+    (.setCellRenderer (create-list-renderer))
+    (.setSelectedIndex 0)))
 
 (defn- switch-panel [frame panel]
   (doto (.getContentPane frame)
@@ -39,7 +59,9 @@
 
 (defn end-fn [frame name result]
   (case result
-	:win (JOptionPane/showMessageDialog frame (str "You win level " name))
+	:win (do
+	       (JOptionPane/showMessageDialog frame (str "You win level " name))
+	       (update-results name :win))
 	:lose (JOptionPane/showMessageDialog frame (str "You lose level " name))
 	nil)
   (switch-panel frame (create-jlist frame)))
@@ -49,7 +71,7 @@
   (proxy [KeyAdapter] []
 	 (keyPressed [e] 
 		   (when (= (.getKeyCode e) (KeyEvent/VK_ENTER))
-			  (let [name (.getSelectedValue (.getSource e))
+			  (let [name (first (.. e getSource getSelectedValue))
 				levels-map (get-levels-map)
 				game-panel (create-game-panel ((levels-map name)) 
 							      (partial end-fn frame name))]
