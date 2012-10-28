47ea1121158f6de14b105c4ec5d72bf7019c9d28
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-10-28 22:52:22 +0300

    Add dependencies.

--- project.clj
+++ project.clj
@@ -1,6 +1,5 @@
 (defproject snakejure "0.1"
   :description "Snake game written in clojure"
-  :dependencies [[org.clojure/clojure "1.2.0-RC1"]
-                 [org.clojure/clojure-contrib "1.2.0-RC1"]
-		 [log4j/log4j "1.2.16"]]
-  :dev-dependencies [[swank-clojure "1.2.0"]])
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 [quil "1.6.0"]
+                 [aleph "0.3.0-beta5"]])
