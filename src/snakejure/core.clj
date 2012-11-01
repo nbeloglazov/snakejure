7cb11d8ffeaec494372d280326df5d2ff76f3d65
Nikita Beloglazov <nikelandjelo@gmail.com>
2012-11-02 01:00:06 +0300

    Add simple chat-like application.

--- src/snakejure/core.clj
+++ src/snakejure/core.clj
@@ -1,1 +1,22 @@
-(ns snakejure.core)
+(ns snakejure.core
+  (:use [lamina.core :only (siphon grounded-channel receive-all enqueue)]
+        [aleph.object :only (start-object-server object-client)]))
+
+(defn empty-handler [message])
+
+(def broadcast (grounded-channel))
+
+(defn handler [channel info]
+  (siphon broadcast channel)
+  (receive-all channel #(enqueue broadcast %)))
+
+(defn start-server []
+  (println "Starting server at port 12345")
+  (start-object-server
+   	handler
+  	{:port 12345}))
+
+(defn start-client []
+  (aleph-obj/object-client
+   {:host "localhost"
+	:port 12345}))
