01edbdde313838f5a24831d19be423d0df77a905
kabbi <userdima@gmail.com>
2012-11-13 20:24:27 +0300

    Almost implemented multiple snakes and fixed indents (do not die while playing, it will kill the server\!)

--- src/snakejure/server.clj
+++ src/snakejure/server.clj
@@ -7,11 +7,19 @@
 
 (def broadcast (grounded-channel))
 (def world (atom test-world))
+(def client-to-snake-map (atom {}))
 
 (defn handler [channel info]
-  (siphon broadcast channel)
-  (receive-all channel
-  	#(swap! world assoc-in [:snakes 0 :dir] %)))
+  (let [occupied (set (concat (:apples @world) (:walls @world) (mapcat :body (:snakes @world))))
+        snake-idx (count (:snakes @world))
+        snake-head (first (core/rand-cells occupied))
+        snake {:body [snake-head] :dir :down}]
+    (siphon broadcast channel)
+    (swap! client-to-snake-map assoc channel snake-idx)
+    (swap! world assoc :snakes (conj (:snakes @world) snake))
+    (println "New snake! Number: " snake-idx " Snake: " snake)
+    (receive-all channel
+      #(swap! world assoc-in [:snakes (@client-to-snake-map channel) :dir] %))))
 
 (def timer (java.util.Timer.))
 
@@ -20,10 +28,10 @@
   (enqueue broadcast @world))
 
 (defn start-server []
-	(let [timer-task (proxy [java.util.TimerTask] []
+  (let [timer-task (proxy [java.util.TimerTask] []
                  (run [] (next-step)))]
-		(println "Starting server at port 12345")
-		(start-object-server
-			handler
-			{:port 12345})
-		(.schedule timer timer-task 0 frequency)))
+    (println "Starting server at port 12345")
+    (.schedule timer timer-task 0 frequency)
+    (start-object-server
+      handler
+      {:port 12345})))
