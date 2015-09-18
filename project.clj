78e992c6eac6cb705afe32d01a12ed9d580cb0b0
lamuria <lamuria@users.noreply.github.com>
2015-09-18 18:49:57 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject snakejure "0.1"
   :description "Snake game written in clojure"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [quil "1.6.0"]
+                 [quil "2.2.6"]
                  [aleph "0.4.0"]])
